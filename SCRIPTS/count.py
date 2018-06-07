003d2cc6aaa689ec06efb932130e201b32ee0eca
Virgile Tellier <37550192+virgile-tellier@users.noreply.github.com>
2018-06-07 16:39:20 +0200

    Add files via upload

--- SCRIPTS/count.py
+++ SCRIPTS/count.py
@@ -1,6 +1,11 @@
+"""
+
+Count drug occurences from input file. Format of said file should be checked.
+"""
+
 from utils import File_Reader as FR
 
-file = FR("../PUBMED_DATA/pubmedNdrugs_2.txt", sep = '\t', suppress_newlines = True, skiplines = 0, encoding = "utf-8")
+file = FR("../PUBMED_DATA/pubmedNdrugs.latest.txt", sep = '\t', suppress_newlines = True, skiplines = 0, encoding = "utf-8")
 
 counter = {}
 
@@ -24,6 +29,6 @@ s = sorted(counter.items(), key=lambda x: x[1])[::-1]
 
 print(s)
 
-with open("../PUBMED_DATA/drug_counter.txt", 'w', encoding = "utf8") as fp:
+with open("../PUBMED_DATA/drug_counter.latest.txt", 'w', encoding = "utf8") as fp:
 	for i in s:
-		fp.write(("\t".join([i[0],str(i[1])])+'\r\n'))
+		fp.write(("\t".join([i[0],str(i[1])])+'\n'))
